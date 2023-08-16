65cb906306b7619b4780641a6d2883ad7d742bba
Domenic Dewald <domenic2610@gmail.com>
2023-08-16 22:42:13 +0200

    Wallpaperupdate and shellcheck added

--- home/configfiles/hypr/scripts/swww.sh
+++ home/configfiles/hypr/scripts/swww.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 swww init
-swww img /home/domenic/Pictures/Wallpapers/wallpaperflare.com_wallpaper.jpg
+swww img /home/domenic/Pictures/Wallpapers/wallpaper.jpg
--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,7 @@
   home.username = "domenic";
   home.homeDirectory = "/home/domenic";
   home.packages = with pkgs; [
+    shellcheck
     grim
     slurp
     lazygit
