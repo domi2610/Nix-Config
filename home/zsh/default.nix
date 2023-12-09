934bb45decb4f09597281a316911d6715a43386c
Domenic Dewald <domenic2610@gmail.com>
2023-12-09 01:35:40 +0100

    updated alias list for zsh

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -8,13 +8,10 @@
       ll = "ls -l --color=auto";
       la = "ls -A --color=auto";
       lla = "ls -lA --color=auto";
-      pipewire-restart =
-        "systemctl --user restart pipewire.service && systemctl --user restart wireplumber";
       webserver-ssh = "ssh ubuntu@144.24.186.232";
       webserver-mount = "sshfs ubuntu@144.24.186.232:/home/ubuntu ~/mnt";
       nix-update = "nix flake update ~/Projects/NixOS-Config/";
       nix-clean = "nix-collect-garbage -d && sudo nix-collect-garbage -d";
-      nix-lock-upload = "git add . && git commit -m 'nix: update' && git push";
       setup-direnv = "echo 'use flake' >> .envrc && direnv allow";
       rebuild-system =
         "sudo nixos-rebuild switch --flake github:domi2610/Nix-Config#nixos";
