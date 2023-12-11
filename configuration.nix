74598c16b65673931f4460870a29a5486c599b2e
Domenic Dewald <domenic2610@gmail.com>
2023-12-11 12:00:28 +0100

    remove electron

--- configuration.nix
+++ configuration.nix
@@ -135,7 +135,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [ "electron-24.8.6" ];
+  nixpkgs.config.permittedInsecurePackages = [ ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
