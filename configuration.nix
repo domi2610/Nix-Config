68bbb413bc29177b36745bfbd137e01b47431968
Domenic Dewald <domenic2610@gmail.com>
2024-01-19 00:35:29 +0100

    back to gnome

--- configuration.nix
+++ configuration.nix
@@ -73,15 +73,8 @@
 
   # Enable the GNOME Desktop Environment.
 
-  # for Hyprland
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.displayManager.lightdm.greeter.enable = false;
-
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
-  # use hyprland window manager
-  programs.hyprland.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
