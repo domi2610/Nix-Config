daaf25847c7596e381a6f1b4d2e3fe987dd80e31
Domenic Dewald <domenic2610@gmail.com>
2024-08-28 22:34:46 +0200

    performance cpu governor

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -49,4 +49,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  powerManagement.cpuFreqGovernor = "performance";
 }
