86951acf4e67697607fcd3266a4aade55551949c
Domenic Dewald <domenic2610@gmail.com>
2024-08-28 17:44:53 +0200

    change autotag in nvim

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -110,7 +110,24 @@ in
         plugin = nvim-ts-autotag;
         type = "lua";
         config = ''
-          require('nvim-ts-autotag').setup()
+                    require('nvim-ts-autotag').setup({
+                        require('nvim-ts-autotag').setup({
+            opts = {
+              -- Defaults
+              enable_close = true, -- Auto close tags
+              enable_rename = true, -- Auto rename pairs of tags
+              enable_close_on_slash = false -- Auto close on trailing </
+            },
+            -- Also override individual filetype configs, these take priority.
+            -- Empty by default, useful if one of the "opts" global settings
+            -- doesn't work well in a specific filetype
+            per_filetype = {
+              ["html"] = {
+                enable_close = false
+              }
+            }
+          })
+                        })
         '';
       }
       nvim-surround
