25d4256cedd4036b8b26b7ae5c7d0b1f791c7d13
Domenic Dewald <domenic2610@gmail.com>
2024-08-28 15:09:16 +0200

    use gdm isntead

--- configuration.nix
+++ configuration.nix
@@ -83,7 +83,7 @@
 
   # Enable the GNOME Desktop Environment.
 
-  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = false;
 
   # or enable Hyprland
